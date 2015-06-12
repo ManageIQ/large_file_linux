4429ce665db09af29bbd23094b558f8c4ace6f02
Jason Frey <fryguy9@gmail.com>
2015-06-12 19:28:56 -0400

    Add more specs

--- spec/large_file_linux_spec.rb
+++ spec/large_file_linux_spec.rb
@@ -1,12 +1,41 @@
 require 'spec_helper'
 
 describe LargeFileLinux do
+  let(:path) { create_large_file }
+
+  after do
+    @lf.close if @lf
+  end
+
   it 'has a version number' do
     expect(LargeFileLinux::VERSION).not_to be nil
   end
 
-  it '#read' do
-    l = LargeFileLinux.new(__FILE__, "r")
-    expect(l.read(21)).to eq("require 'spec_helper'")
+  it '#size' do
+    @lf = LargeFileLinux.new(path, "r")
+    expect(@lf.size).to eq(TOTAL_SIZE)
+  end
+
+  it '#close' do
+    lf = LargeFileLinux.new(path, "r")
+    expect { lf.close }.to_not raise_error
+  end
+
+  it '#read / #seek' do
+    @lf = LargeFileLinux.new(path, "r")
+    expect(@lf.read(5)).to eq("\0\0\0\0\0")
+
+    seek_to_extra_data(@lf)
+    expect(@lf.read(EXTRA_DATA.length)).to eq(EXTRA_DATA)
+  end
+
+  it '#write / #seek' do
+    @lf = LargeFileLinux.new(path, "r+")
+
+    seek_to_extra_data(@lf)
+    @lf.write(EXTRA_DATA.reverse, EXTRA_DATA.length)
+
+    seek_to_extra_data(@lf)
+    expect(@lf.read(EXTRA_DATA.length)).to eq(EXTRA_DATA.reverse)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,26 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'large_file_linux'
+
+FOUR_GB    = 4_294_967_296 # 4 * 1024 * 1024 * 1024
+INT32_MAX  = 2_147_483_647 # 2 * 1024 * 1024 * 1024 - 1
+EXTRA_DATA = "abcdefghijklnopqrstuvwxyz0123456789"
+TOTAL_SIZE = FOUR_GB + EXTRA_DATA.length + 1 # + 1 for the trailing \n
+
+def create_large_file
+  require 'tempfile'
+  File.join(Dir.tmpdir, "test_large_file").tap do |path|
+    `truncate --size #{FOUR_GB} "#{path}"`
+    `echo "#{EXTRA_DATA}" >> "#{path}"`
+  end
+end
+
+def seek_to_extra_data(lf)
+  # LargeFileLinux expects to be run on a 32 bit Linux, where int is 32 bit.
+  #   Thus, we can only seek INT32_MAX at most if we are testing on a 64 bit
+  #   machine.
+  lf.seek(0, IO::SEEK_SET)
+  2.times do
+    lf.seek(INT32_MAX, IO::SEEK_CUR)
+    lf.seek(1, IO::SEEK_CUR)
+  end
+end
